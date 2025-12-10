5c46f1b74d6d8d5ab5a3563ad447a8f5a374df40
Wenzhen Wang <wanwenzh@sheridancollege.ca>
2025-12-09 19:12:47 -0500

    added docs and tests

---
+++ docs/issues.md
@@ -0,0 +1,10 @@
+ID:             ISSUE #1 | Final Project
+TITLE:          Request additional input field
+TYPE:           🔧 Enhancement
+PRIORITY:       🟢 Low
+STATUS:         📋 Backlog
+ASSIGNEE:       Wenzhen Wang
+REPORTER:       Wenzhen Wang
+CREATED DATE:   9/12/2025
+DUE DATE:       14/12/2025
+
--- src/test/java/ca/sheridancollege/wenzh/Lec82ResTfulAngularApplicationTests.java
+++ src/test/java/ca/sheridancollege/wenzh/Lec82ResTfulAngularApplicationTests.java
@@ -1,13 +1,32 @@
 package ca.sheridancollege.wenzh;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class Lec82ResTfulAngularApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void testGetAllStudents() throws Exception {
+        mockMvc.perform(get("/api/v1/students")).andExpect(status().isOk());
+    }
+
+	@Test
+    void testHealth() throws Exception {
+        mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"));
+    }
 }
